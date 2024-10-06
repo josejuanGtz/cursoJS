a6b6c631f21d940d068acecf81da9d1d59e5484c
josejuanGtz <josejuangutierrezsolorio@gmail.com>
2024-10-06 01:56:18 -0600

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+//imprimir mensaje desde consola
+console.log("Hola mundo");
+
+//pedir nombre con prompt
+var nombre = prompt("¿Cuál es tu nombre?");
+
+//imprimir mensaje desde alert
+alert("Hola " + nombre + " ¿Cómo estas?");
