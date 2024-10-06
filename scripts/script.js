55a552481d7260cb1d5d5b64a8f151b82f1f3d78
josejuanGtz <josejuangutierrezsolorio@gmail.com>
2024-10-06 05:58:30 -0600

    Update script.js
    - template string backticks
    - ciclo while
    - ciclo do while
    - ciclo for
    - ciclo foreach
    - ordenamiento sort

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,35 @@ console.log("Hola mundo");
 var nombre = prompt("¿Cuál es tu nombre?");
 
 //imprimir mensaje desde alert
-alert("Hola " + nombre + " ¿Cómo estas?");
+/*las comillas simples usadas aqui se llaman backticks y sirven para concatenar variables 
+dentro de ellas sin necesidad de cortar la cadena*/
+alert(`Hola ${nombre} ¿Cómo estas?`);
+
+//ciclo while --> se repite MIENTRAS la condición sea verdadera, ejecuta la condicion antes de la iteración
+let indice = 1;
+while (indice<10) {
+    console.log("WHILE el valor es: " + indice);
+    indice++;
+}
+
+//ciclo do while --> se repite MIENTRAS la condición sea verdadera, ejecuta la condicion después de la iteración
+indice = 1;
+do{
+    console.log("DO WHILE el valor es: " + indice);
+    indice++;
+}while (indice<10) 
+
+//ciclo for
+let lista = [33,6,9];
+for (let index = 0; index < lista.length; index++) {
+    console.log("FOR el valor es: " + lista[index]);
+    
+}
+
+//ciclo foreach
+lista.forEach(element => {
+    console.log("FOREACH el valor es: " + element);
+});
+
+//ordenamiento sort
+console.log(lista.sort());
